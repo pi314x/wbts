0be54232b12538ae9c245d0534c75fee8c8efb37
pi314x <51694688+pi314x@users.noreply.github.com>
2023-05-27 13:27:50 +0200

    Update app.js

--- app.js
+++ app.js
@@ -235,7 +235,8 @@ async function BitShares() {
   var Apis = await bitshares_js.bitshares_ws.Apis; 
   await Apis.instance().connect(node);
   let obj = await Apis.instance().db_api().exec("get_objects", [['1.3.0']]);
-  let ticker = await Apis.instance().db_api().exec("get_ticker", ['1.3.0','1.3.5589']);
+  //let ticker = await Apis.instance().db_api().exec("get_ticker", ['1.3.0','1.3.5589']);
+  let ticker = await Apis.instance().db_api().exec("get_ticker", ['1.3.0','1.3.22']);
   let balances = await Apis.instance().db_api().exec("get_objects", [CUSTODIAN,["1.3.0"]]);
   var total = Number(balances[0]["amount"]);
   let symbol = obj[0]['symbol'];

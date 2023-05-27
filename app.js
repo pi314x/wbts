8de453b6a6f76b86e22d8eeef5792dadfdbb652b
pi314x <51694688+pi314x@users.noreply.github.com>
2023-05-28 00:50:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -226,6 +226,9 @@ async function BitShares() {
 	
 	ws.onopen = function (event) {
       console.log('connection is open!')
+if (ws.readyState  === 1) {
+     console.log("It is safe to send messages now")
+}
  ws.send('{"id":1, "method":"call", "params":[0,"get_objects", [["1.3.0","'+CUSTODIAN+'"]]]}');
 	ws.send('{"id":2, "method":"call", "params":[0,"get_ticker", ["1.3.0","1.3.22"]]}');
 	ws.send('{"id":3, "method":"call", "params":[0,"get_account_balances", ["'+CUSTODIAN+'",["1.3.0"]]]}')

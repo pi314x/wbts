c0663aca4d26a9963e20c25b2790a02ed71b6fed
pi314x <51694688+pi314x@users.noreply.github.com>
2023-05-27 09:56:49 +0200

    Update app.js

--- app.js
+++ app.js
@@ -179,13 +179,43 @@ async function totalSupply() {
 
 async function BitShares(node) {
   // https://github.com/bitshares/bitsharesjs
-  bitshares_js.bitshares_ws.Apis.instance(node, true).init_promise.then((res) => {
+  /*bitshares_js.bitshares_ws.Apis.instance(node, true).init_promise.then((res) => {
     console.log("connected to:", res[0].network);
-  });
-  let balances = await bitshares_js.bitshares_ws.Apis.db.get_account_balances(CUSTODIAN,["1.3.0"]);
+  });*/
+	
+	async function get_objects(obj) {
+    return bitshares_js.bitshares_ws.Apis.instance(node, true).db_api().exec("get_objects", [
+                obj
+            ]).then(dict => {
+                return dict;
+            }).catch(err => {
+                console.log("err:", err);
+    })
+}
+
+async function get_ticker(base, quote) {
+    return bitshares_js.bitshares_ws.Apis.instance(node, true).db_api().exec("get_ticker", [
+                base, quote
+            ]).then(dict => {
+                return dict;
+            }).catch(err => {
+                console.log("err:", err);
+    })
+} 
+
+async function get_account_balances(account_id, assets) {
+    return bitshares_js.bitshares_ws.Apis.instance(node, true).db_api().exec("get_account_balances", [
+                account_id , assets
+            ]).then(dict => {
+                return dict;
+            }).catch(err => {
+                console.log("err:", err);
+    })
+} 
+  let balances = await get_account_balances(CUSTODIAN,["1.3.0"]);
   /*var ticker = await bitshares_js.bitshares_ws.Apis.db.get_ticker('1.3.0','1.3.5589');*/
-  let ticker = await bitshares_js.bitshares_ws.Apis.db.get_ticker('1.3.0','1.3.22');
-  let obj = await bitshares_js.bitshares_ws.Apis.db.get_objects(['1.3.0']);
+  let ticker = await get_ticker('1.3.0','1.3.22');
+  let obj = await get_objects(['1.3.0']);
   var total = Number(balances[0]["amount"]);
   let symbol = obj[0]['symbol'];
   let decimals = obj[0]['precision'];

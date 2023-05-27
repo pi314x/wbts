52523d4f546290dd3ac3881824b8d1a0fcf4301a
pi314x <51694688+pi314x@users.noreply.github.com>
2023-05-27 18:40:55 +0200

    Update app.js

--- app.js
+++ app.js
@@ -240,7 +240,7 @@ async function BitShares() {
     console.log("connected to:", res[0].network);
   });
 	
-	Apis = await bitshares_js.bitshares_ws.Apis.instance(node, true);
+	Apis = await bitshares_js.bitshares_ws.Apis.instance(node, true).init_promise;
 	var obelix = await Apis.db_api().exec("get_objects", [['1.3.0']]);
 	var asterix = await Apis.db_api().exec("get_ticker", ['1.3.0','1.3.22']);
   var obj = await get_objects(['1.3.0']);

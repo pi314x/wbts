2cfe688c3fcd381420614c99191c1772828e56f2
pi314x <51694688+pi314x@users.noreply.github.com>
2023-05-31 19:28:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -417,7 +417,7 @@ function copyToClipboard() {
   navigator.clipboard.writeText(copyText).then(() => {
       // Alert the user that the action took place.
       // Nobody likes hidden stuff being done under the hood!
-      alert("Copied to clipboard");
+      // alert("Copied to clipboard");
   });
 }
 

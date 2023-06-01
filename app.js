007ca10104d8267b260f38f2589f32c7297be5a8
pi314x <51694688+pi314x@users.noreply.github.com>
2023-06-01 11:59:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -287,25 +287,21 @@ async function unwrap() {
   let amount = document.getElementById("amount");
   if (wallet.value === "") {
     wallet.style.border = "2px solid #880808";
-    wallet.style.color = "#cc1100";
     wallet.setAttribute("placeholder", "Wallet mustn't be empty.");
     return;
   }
   if (amount.value == null || amount.value === "") {
     amount.style.border = "2px solid #cc1100";
-    amount.style.color = "#cc1100";
     amount.setAttribute("placeholder", "Amount mustn't be empty.");
     return;
   }
   if (amount.value < 0) {
     amount.style.border = "2px solid #cc1100";
-    amount.style.color = "#cc1100";
     amount.setAttribute("placeholder", "Amount must be positive.");
     return;
   }
   if (amount.value == 1) {
     amount.style.border = "2px solid #cc1100";
-    amount.style.color = "#cc1100";
     amount.setAttribute("placeholder", "Amount too low.");
     return;
   }

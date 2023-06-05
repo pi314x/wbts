3fb18ed80b916024f1be6b4180de5a7e82634a5f
pi314x <51694688+pi314x@users.noreply.github.com>
2023-06-05 21:35:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -217,7 +217,7 @@ async function eth() {
     try {
       const res = await window.ethereum.request({
         method: "wallet_switchEthereumChain",
-        params: [{ chainId: "0xaa36a7" }],
+        params: [{ chainId: ethereum.chainId }],
       });
     } catch (switchError) {
       if (switchError.code === 4902) {

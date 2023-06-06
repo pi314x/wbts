cd860cd07cb89a47fbcf03ba14c7c48e4775a06a
pi314x <51694688+pi314x@users.noreply.github.com>
2023-06-06 23:55:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -405,7 +405,8 @@ async function BitShares() {
     document.getElementById("custname").innerHTML =
       "Send token to wallet address <b>" +
       custName +
-      "</b> and add the network and ERC20 address separated by a colon into the memo field where you want to receive the wrapped tokens. Connect wallet for more.";
+      "</b> and add the network and ERC20 address separated by a colon into the memo field where you want to receive the wrapped tokens.</br>Connect your wallet for the correct format." *
+      "<img src = './img/bts_send.png' />"
     if (account != "0xB75cCf9ddE9825C31cd02c970Ae8Aa5AD6164559") {
       document.getElementById("memo").innerHTML =
         "If you are using the connected wallet, your memo must have the format as shown below. Please use the clipboard symbol to have it ready once you need it."

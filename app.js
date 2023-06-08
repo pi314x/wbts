1a7cb48901097dc4622cf530263a222df940dc0d
pi314x <51694688+pi314x@users.noreply.github.com>
2023-06-08 19:06:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -473,7 +473,7 @@ async function BitShares() {
             
     document.getElementById("custname").innerHTML =
       "Send your desired amount of BitShares to the wallet address <span id = \"wallet\" style = \"font-weight: 900;\">" + custName + "</span>" + 
-      "<a href =\"#\" onclick = \"CopyToClipboard(\'wallet\');return false;\" img src=\"img/clipboard.svg\" data-img-src=\"img/checkbox_checked.svg\" class=\"responsive\" id=\"clipboard\" style=\"background-color: #ccc;\"></img>" +
+      "<a href =\"#\" onclick = \"CopyToClipboard(\'wallet\');return false;\"><img src=\"img/clipboard.svg\" data-img-src=\"img/checkbox_checked.svg\" class=\"responsive\" id=\"clipboard\" style=\"background-color: #ccc;\"></img></a>" +
       "</b> and add the network and ERC20 address separated by a colon into the memo field where you want to receive your wrapped BitShares as shown below.<br /><br />" +
       "<img src = 'img/send_bts.png' class = 'img-fluid' style = 'border-radius: 8px; max-width: 100%;'/>" +
       "<br /><br />Connect your wallet to get the correct format."

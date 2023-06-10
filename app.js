07f22a70119a122cec1019e2cc55d1665d317ce0
pi314x <51694688+pi314x@users.noreply.github.com>
2023-06-10 11:33:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -638,6 +638,17 @@ async function main() {
 //https://onebite.dev/play-with-supabase-database-in-website-with-javascript/
 async function ServiceData() {
   
+    const { w_data, w_error } = await _supabase
+        .from('wrapper_status')
+        .select()
+    
+    console.log(w_data)
+  
+    if (!w_error) {
+      const wrapper_running = w_data['data'][0]['running']
+      console.log(wrapper_running)
+    }
+  
     const { data, error } = await _supabase
             .from('unwrapper_status')
             .select()
@@ -653,7 +664,7 @@ async function ServiceData() {
         let contents = ''
         contents += `<div>Please find below the system status of the wrapper & unwrapper services:</div><p></p>` 
         data.forEach(function(item){
-            contents += `<div>${emojis[item.running]} ${item.name} (${item.short_name})</div>` 
+            contents += `<div>${emojis[wrapper_running*item.running]} ${item.name} (${item.short_name})</div>` 
         })
         
         parent.insertAdjacentHTML('beforeend', contents)
